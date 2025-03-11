99d8e35b69ac11c866c8164eab24ee832a5d654f
abarolli <obarolli@gmail.com>
2025-03-11 14:30:41 -0700

    Added attribute converter for enum types

--- src/main/java/io/onicodes/issue_tracker/models/Issue.java
+++ src/main/java/io/onicodes/issue_tracker/models/Issue.java
@@ -1,10 +1,95 @@
 package io.onicodes.issue_tracker.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Converter;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AccessLevel;
+
+
+enum IssueStatus {
+    OPEN,
+    IN_PROGRESS,
+    RESOLVED,
+    CLOSED
+}
+
+
+enum IssuePriority {
+    LOW,
+    MEDIUM,
+    HIGH,
+    CRITICAL
+}
+
+
+@Converter(autoApply = true)
+class LowerCaseDBDataToUpperCaseAttributeConverter<T extends Enum<T>> implements AttributeConverter<T, String> {
+
+    private Class<T> enumType;
+
+    public LowerCaseDBDataToUpperCaseAttributeConverter(Class<T> enumType) {
+        this.enumType = enumType;
+    }
+
+    @Override
+    public String convertToDatabaseColumn(T attribute) {
+        return attribute != null ? attribute.name().toLowerCase() : null;
+    }
+
+    @Override
+    public T convertToEntityAttribute(String dbData) {
+        try {
+            return dbData != null ? Enum.valueOf(enumType, dbData.toUpperCase()) : null;
+        }
+        catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(
+                "Unknown value for enum " + enumType.getSimpleName() + ": " + dbData);
+        }
+    }
+
+}
+
+
+@Converter(autoApply = true)
+class IssueStatusConverter extends LowerCaseDBDataToUpperCaseAttributeConverter<IssueStatus> {
+
+    public IssueStatusConverter() {
+        super(IssueStatus.class);
+    }
+
+}
+
+
+@Converter(autoApply = true)
+class IssuePriorityConverter extends LowerCaseDBDataToUpperCaseAttributeConverter<IssuePriority> {
+
+    public IssuePriorityConverter() {
+        super(IssuePriority.class);
+    }
+
+}
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 public class Issue {
 
