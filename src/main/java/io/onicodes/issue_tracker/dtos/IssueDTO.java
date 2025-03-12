93f49d29ed1d1e4c2be95fe76920df5c81c1e32a
abarolli <obarolli@gmail.com>
2025-03-12 16:03:40 -0700

    Added additional lombok annotations to DTOs

--- src/main/java/io/onicodes/issue_tracker/dtos/IssueDTO.java
+++ src/main/java/io/onicodes/issue_tracker/dtos/IssueDTO.java
@@ -4,13 +4,14 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
 public class IssueDTO {
     private Long id;
     private String title;
--- src/main/java/io/onicodes/issue_tracker/dtos/UserDTO.java
+++ src/main/java/io/onicodes/issue_tracker/dtos/UserDTO.java
@@ -1,6 +1,8 @@
 package io.onicodes.issue_tracker.dtos;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -10,6 +12,8 @@ import io.onicodes.issue_tracker.models.User;
 @Getter
 @Setter
 @ToString
+@EqualsAndHashCode
+@NoArgsConstructor
 public class UserDTO {
     private Long id;
     private String name;
