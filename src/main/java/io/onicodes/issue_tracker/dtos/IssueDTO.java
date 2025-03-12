e73ec9fe5a1a520ec9624e0d1476c97cfcdecaf8
abarolli <obarolli@gmail.com>
2025-03-12 14:14:48 -0700

    Removed unneeded constructor

--- src/main/java/io/onicodes/issue_tracker/dtos/IssueDTO.java
+++ src/main/java/io/onicodes/issue_tracker/dtos/IssueDTO.java
@@ -2,38 +2,22 @@ package io.onicodes.issue_tracker.dtos;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import lombok.Getter;
 import lombok.Setter;
-
-import io.onicodes.issue_tracker.models.issue.Issue;
-import io.onicodes.issue_tracker.models.issue.IssuePriority;
-import io.onicodes.issue_tracker.models.issue.IssueStatus;
+import lombok.ToString;
 
 
 @Getter
 @Setter
+@ToString
 public class IssueDTO {
     private Long id;
     private String title;
     private String description;
-    private IssueStatus status;
-    private IssuePriority priority;
+    private String status;
+    private String priority;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private List<UserDTO> assignees;
-
-    public IssueDTO(Issue issue) {
-        this.id = issue.getId();
-        this.title = issue.getTitle();
-        this.description = issue.getDescription();
-        this.status = issue.getStatus();
-        this.priority = issue.getPriority();
-        this.createdAt = issue.getCreatedAt();
-        this.updatedAt = issue.getUpdatedAt();
-        this.assignees = issue.getAssignees().stream()
-            .map(a -> new UserDTO(a.getUser()))
-            .collect(Collectors.toList());
-    }
 }
