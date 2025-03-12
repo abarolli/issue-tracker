855d7df4f6abe2d6225e9935e9594b043c4a9f08
abarolli <obarolli@gmail.com>
2025-03-12 14:14:27 -0700

    ToString annotation on UserDTO

--- src/main/java/io/onicodes/issue_tracker/dtos/UserDTO.java
+++ src/main/java/io/onicodes/issue_tracker/dtos/UserDTO.java
@@ -1,11 +1,15 @@
 package io.onicodes.issue_tracker.dtos;
 
-import io.onicodes.issue_tracker.models.User;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+
+import io.onicodes.issue_tracker.models.User;
+
 
 @Getter
 @Setter
+@ToString
 public class UserDTO {
     private Long id;
     private String name;
