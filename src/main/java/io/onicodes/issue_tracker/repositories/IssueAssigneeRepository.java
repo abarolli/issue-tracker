e04013c36ead38be5fb7b733c1005dceb4939453
abarolli <obarolli@gmail.com>
2025-03-13 11:31:22 -0700

    Renamed repos for consistency with other beans

--- src/main/java/io/onicodes/issue_tracker/repositories/IssueAssigneeRepository.java
+++ src/main/java/io/onicodes/issue_tracker/repositories/IssueAssigneeRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import io.onicodes.issue_tracker.models.issueAssignee.IssueAssignee;
 import io.onicodes.issue_tracker.models.issueAssignee.IssueAssigneeId;
 
-public interface IssueAssigneesRepository extends JpaRepository<IssueAssignee, IssueAssigneeId> {
+public interface IssueAssigneeRepository extends JpaRepository<IssueAssignee, IssueAssigneeId> {
 
 }
--- src/main/java/io/onicodes/issue_tracker/repositories/UserRepository.java
+++ src/main/java/io/onicodes/issue_tracker/repositories/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.onicodes.issue_tracker.models.User;
 
-public interface UsersRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
