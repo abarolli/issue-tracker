87fac6e9193f5018289d37b58758630ff3522024
abarolli <obarolli@gmail.com>
2025-03-13 11:13:06 -0700

    Removed constructor mapping

--- src/main/java/io/onicodes/issue_tracker/dtos/UserDTO.java
+++ src/main/java/io/onicodes/issue_tracker/dtos/UserDTO.java
@@ -6,9 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import io.onicodes.issue_tracker.models.User;
-
-
 @Getter
 @Setter
 @ToString
@@ -18,10 +15,4 @@ public class UserDTO {
     private Long id;
     private String name;
     private String email;
-
-    public UserDTO(User user) {
-        this.id = user.getId();
-        this.name = user.getName();
-        this.email = user.getEmail();
-    }
 }
