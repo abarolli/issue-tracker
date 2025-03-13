c8a2aed1e0fdd2eef49269268089108ffa3d48c1
abarolli <obarolli@gmail.com>
2025-03-13 14:22:11 -0700

    Using IssueService for get requests

--- src/main/java/io/onicodes/issue_tracker/controllers/IssuesController.java
+++ src/main/java/io/onicodes/issue_tracker/controllers/IssuesController.java
@@ -1,9 +1,8 @@
 package io.onicodes.issue_tracker.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,36 +11,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.onicodes.issue_tracker.controllers.exceptions.IssueNotFoundException;
 import io.onicodes.issue_tracker.dtos.IssueDTO;
-import io.onicodes.issue_tracker.models.issue.Issue;
-import io.onicodes.issue_tracker.repositories.IssuesRepository;
+import io.onicodes.issue_tracker.services.IssueService;
 
 
 @RestController
 @RequestMapping("/issues")
 public class IssuesController {
-    private final IssuesRepository issuesRepository;
 
-
-    public IssuesController(IssuesRepository issuesRepository) {
-        this.issuesRepository = issuesRepository;
-    }
+    @Autowired
+    private IssueService issueService;
 
     @GetMapping
     public Page<IssueDTO> getIssues(@RequestParam(defaultValue = "0") int page,
-                                 @RequestParam(defaultValue = "10") int size) {
+                                    @RequestParam(defaultValue = "10") int size) {
 
-        Pageable pageable = PageRequest.of(page, size);
-        return issuesRepository.findAll(pageable)
-            .map(IssueDTO::new);
+        return issueService.getIssues(page, size);
     }
 
     @GetMapping("/{id}")
     public IssueDTO getIssue(@PathVariable Long id) {
-        return issuesRepository.findById(id)
-            .map(IssueDTO::new)
-            .orElseThrow(() -> new IssueNotFoundException(id));
+        return issueService.getIssue(id);
     }
 
     @PostMapping
