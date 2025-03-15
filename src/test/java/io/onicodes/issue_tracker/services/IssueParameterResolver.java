456868d5723689cace38853ecf6d8f774abced63
abarolli <obarolli@gmail.com>
2025-03-14 19:19:46 -0700

    Added IssueParameterResolver

---
+++ src/test/java/io/onicodes/issue_tracker/services/IssueParameterResolver.java
@@ -0,0 +1,38 @@
+package io.onicodes.issue_tracker.services;
+
+import java.util.List;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+import io.onicodes.issue_tracker.dtos.UserDTO;
+import io.onicodes.issue_tracker.dtos.issue.IssueCreateRequestDTO;
+
+public class IssueParameterResolver implements ParameterResolver {
+
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
+        
+        var parameterType = parameterContext.getParameter().getType();
+        return parameterType.equals(IssueCreateRequestDTO.class);
+    }
+
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
+        var issueCreateRequestDTO = new IssueCreateRequestDTO();
+        issueCreateRequestDTO.setTitle("test title");
+        issueCreateRequestDTO.setDescription("test desc");
+        issueCreateRequestDTO.setStatus("OPEN");
+        issueCreateRequestDTO.setPriority("CRITICAL");
+
+        var user = new UserDTO();
+        user.setId(Long.valueOf(1));
+        user.setName("Oni");
+        user.setEmail("myemail@gmail.com");
+        
+        issueCreateRequestDTO.setAssignees(List.of(user));
+        return issueCreateRequestDTO;
+    }
+    
+}
