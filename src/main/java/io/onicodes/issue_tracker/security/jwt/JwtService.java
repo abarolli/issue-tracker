fdd8bc03eca7d9275e9a0b8ef6c79d6b8d28625a
abarolli <obarolli@gmail.com>
2025-03-18 15:09:45 -0700

    Add JwtService for token generation and validation

---
+++ src/main/java/io/onicodes/issue_tracker/security/jwt/JwtService.java
@@ -0,0 +1,57 @@
+package io.onicodes.issue_tracker.security.jwt;
+
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+
+
+@Service
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+    private SecretKey key;
+
+    @PostConstruct // needed because SECRET_KEY is injected after field initialization
+    public void init() {
+        key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+    
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token, String username) {
+        return username.equals(extractUsername(token)) && !isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration()
+                .before(new Date());
+    }
+}
