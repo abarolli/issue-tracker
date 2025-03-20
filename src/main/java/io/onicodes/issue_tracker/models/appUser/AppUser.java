654738d3fdcfa62e25f3e374c59712cec22b9a63
abarolli <obarolli@gmail.com>
2025-03-20 15:42:48 -0700

    Add Role entity and relational field on AppUser

--- src/main/java/io/onicodes/issue_tracker/models/appUser/AppUser.java
+++ src/main/java/io/onicodes/issue_tracker/models/appUser/AppUser.java
@@ -9,6 +9,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -48,4 +51,12 @@ public class AppUser {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<IssueAssignee> issues = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+        name = "user_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
 }
