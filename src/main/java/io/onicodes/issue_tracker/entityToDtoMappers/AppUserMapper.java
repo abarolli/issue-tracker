5fc4b54b25f752601ca8ab8bcf8df7ad28770ffe
abarolli <obarolli@gmail.com>
2025-03-20 15:41:59 -0700

    Add mapping for RegistrationRequestDto to AppUser in AppUserMapper

--- src/main/java/io/onicodes/issue_tracker/entityToDtoMappers/AppUserMapper.java
+++ src/main/java/io/onicodes/issue_tracker/entityToDtoMappers/AppUserMapper.java
@@ -7,6 +7,7 @@ import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import io.onicodes.issue_tracker.dtos.AppUserDto;
+import io.onicodes.issue_tracker.dtos.RegistrationRequestDto;
 import io.onicodes.issue_tracker.models.appUser.AppUser;
 
 @Mapper
@@ -16,6 +17,12 @@ public interface AppUserMapper {
     @Mapping(target = "issues", ignore = true)
     @Mapping(target = "password", ignore = true)
     AppUser appUserDtoToAppUser(AppUserDto userDto);
+    
+    AppUserDto appUserToAppUserDto(AppUser user);
 
     List<AppUser> appUserDtoListToAppUserList(List<AppUserDto> userDtos);
+
+    @Mapping(target = "issues", ignore = true)
+    @Mapping(target = "roles", ignore = true)
+    AppUser registrationRequestDtoToAppUser(RegistrationRequestDto requestDto);
 }
