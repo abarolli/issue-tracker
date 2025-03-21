f1c3a424b37a66bd89e5836af3c51af619dec851
abarolli <obarolli@gmail.com>
2025-03-20 19:24:09 -0700

    Implement extractRoles to be used during authentication

--- src/main/java/io/onicodes/issue_tracker/security/jwt/JwtService.java
+++ src/main/java/io/onicodes/issue_tracker/security/jwt/JwtService.java
@@ -1,14 +1,18 @@
 package io.onicodes.issue_tracker.security.jwt;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.crypto.SecretKey;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import io.onicodes.issue_tracker.entityToDtoMappers.AppUserMapper;
+import io.onicodes.issue_tracker.repositories.AppUserRepository;
 import jakarta.annotation.PostConstruct;
 
 
@@ -16,7 +20,11 @@ import jakarta.annotation.PostConstruct;
 public class JwtService {
     @Value("${jwt.secret}")
     private String SECRET_KEY;
+    
     private SecretKey key;
+    
+    @Autowired
+    private AppUserRepository userRepository;
 
     @PostConstruct // needed because SECRET_KEY is injected after field initialization
     public void init() {
@@ -24,9 +32,12 @@ public class JwtService {
     }
     
     public String generateToken(String username) {
+        var user = userRepository.findByUsername(username);
+        var roles = AppUserMapper.INSTANCE.roleSetToStringList(user.getRoles());
         final int oneHour = 1000 * 60 * 60;
         return Jwts.builder()
                 .subject(username)
+                .claim("roles", roles)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + oneHour))
                 .signWith(key)
@@ -40,6 +51,15 @@ public class JwtService {
                 .parseSignedClaims(token)
                 .getPayload()
                 .getSubject();
+    }
+
+    public List<String> extractRoles(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("roles", List.class);
     }
 
     public boolean validateToken(String token, String username) {
