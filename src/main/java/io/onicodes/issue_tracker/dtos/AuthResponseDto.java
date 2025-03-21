d7deef6d9a581224b7d6b7f7204a0a1f3e1736aa
abarolli <obarolli@gmail.com>
2025-03-20 19:27:40 -0700

    Add DTOs for authentication and registration requests

---
+++ src/main/java/io/onicodes/issue_tracker/dtos/AuthResponseDto.java
@@ -0,0 +1,14 @@
+package io.onicodes.issue_tracker.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AuthResponseDto {
+    private String token;
+}
