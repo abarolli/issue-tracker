2b63793a9a64cf5a41ef027b93f5b74d9f6ea291
abarolli <obarolli@gmail.com>
2025-03-20 19:26:25 -0700

    Refactor IssueMapper to use AppUserMapper for user DTO conversion

--- src/main/java/io/onicodes/issue_tracker/entityToDtoMappers/IssueMapper.java
+++ src/main/java/io/onicodes/issue_tracker/entityToDtoMappers/IssueMapper.java
@@ -10,7 +10,6 @@ import org.mapstruct.factory.Mappers;
 import io.onicodes.issue_tracker.dtos.AppUserDto;
 import io.onicodes.issue_tracker.dtos.issue.IssueRequestDto;
 import io.onicodes.issue_tracker.dtos.issue.IssueResponseDto;
-import io.onicodes.issue_tracker.models.appUser.AppUser;
 import io.onicodes.issue_tracker.models.issue.Issue;
 import io.onicodes.issue_tracker.models.issueAssignee.IssueAssignee;
 
@@ -19,6 +18,7 @@ import io.onicodes.issue_tracker.models.issueAssignee.IssueAssignee;
 public interface IssueMapper {
 
     IssueMapper INSTANCE = Mappers.getMapper(IssueMapper.class);
+    AppUserMapper userMapper = Mappers.getMapper(AppUserMapper.class);
     
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "assignees", ignore = true)
@@ -35,10 +35,8 @@ public interface IssueMapper {
 
     IssueResponseDto issueToIssueDto(Issue issue);
 
-    AppUserDto userToUserDto(AppUser user);
-
     default AppUserDto issueAssigneeToUserDto(IssueAssignee assignee) {
-        return userToUserDto(assignee.getUser());
+        return userMapper.appUserToAppUserDto(assignee.getUser());
     }
 
 }
